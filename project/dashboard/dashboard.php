03a85783489af0dea3407e57bf1164df34d59f80
Andrea Zavatta <zavattaandrea@gmail.com>
2024-10-31 10:41:16 +0100

    change the view based on session of the user
    if the user is logged it can load data, view the profile data and change the password. if not logged, it can also view the map and the votes

--- project/dashboard/dashboard.php
+++ project/dashboard/dashboard.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    require "../db_connection.php";
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -12,102 +17,92 @@
         <h2>Dashboard</h2>
 
         <!-- Spinner Container -->
-        <div id="loading-spinner" class="spinner">
+        <div id="loading-spinner" class="spinner" style="display: none;">
             <div class="loader"></div>
             <p id="progress-text">Caricamento dati... 0%</p>
-            <button id="stop-import-btn" style="margin-top: 10px;">Interrompi Importazione</button> <!-- Stop Import Button -->
+            <button id="stop-import-btn" style="margin-top: 10px;">Interrompi Importazione</button>
         </div>
 
-        <!-- Menu Tabs -->
-        <div class="tab-container">
-            <span class="tab" onclick="showTab(0)">Carica CSV</span>
-            <span class="tab" onclick="showTab(1)">Profilo</span>
-            <span class="tab" onclick="showTab(2)">Cambia Password</span>
-        </div>
+        <?php if (isset($_SESSION['admin_id'])): ?>
+            <!-- Menu Tabs -->
+            <div class="tab-container">
+                <span class="tab" onclick="showTab(0)">Carica CSV</span>
+                <span class="tab" onclick="showTab(1)">Profilo</span>
+                <span class="tab" onclick="showTab(2)">Cambia Password</span>
+            </div>
 
-        <!-- Tab contenuto: Caricamento CSV -->
-<div class="tab-content">
-            <h3>Carica file CSV</h3>
-            
-            <!-- Section for Application 1 -->
-            <fieldset>
-                <legend>Mappa</legend>
-                <form id="upload-form" onsubmit="event.preventDefault(); uploadFile();">
-                    <label for="csv_file_app1">Seleziona il file CSV per visualizzare i dati sulla mappa</label>
-                    <input type="file" id="csv_file_app1" name="csv_file" accept=".csv" required>
-                    <button type="submit">Carica File per mappa</button>
-                
+            <div class="tab-content">
+                <h3>Carica file CSV</h3>
+                <!-- Section for Application 1 -->
+                <fieldset>
+                    <legend>Mappa</legend>
+                    <form id="upload-form" onsubmit="event.preventDefault(); uploadFile();">
+                        <label for="csv_file_app1">Seleziona il file CSV per visualizzare i dati sulla mappa</label>
+                        <input type="file" id="csv_file_app1" name="csv_file" accept=".csv" required>
+                        <button type="submit">Carica File per mappa</button>
+                    </form>
+                    <div id="delete_container">
+                        <button id="delete-table-btn" onclick="deleteAllTablesMap();">Elimina Tabelle per mappa</button>
+                    </div>
+                </fieldset>
+
+                <!-- Section for Application 2 -->
+                <fieldset>
+                    <legend>Votazioni</legend>
+                    <form id="upload-form-app2" method="POST" enctype="multipart/form-data">
+                        <label for="json_file">Seleziona il file JSON per visualizzare le votazioni:</label>
+                        <input type="file" id="json_file" name="json_file" accept=".json" required>
+                        <button type="submit">Carica File per votazioni</button>
+                    </form>
+                    <div id="delete_container">
+                        <button id="delete-table-btn-app2" onclick="deleteAllTablesVotazioni();">Elimina Tabelle votazioni</button>
+                    </div>
+                </fieldset>
+            </div>
+
+            <!-- Tab contenuto: Profilo -->
+            <div class="tab-content">
+                <h3>Il tuo profilo</h3>
+                <p>Username:
+                    <?php
+                        $adminId = $_SESSION['admin_id'];
+                        $query = "SELECT username FROM admin WHERE id = ?";
+                        $stmt = $connection->prepare($query);
+                        $stmt->bind_param('i', $adminId);
+                        $stmt->execute();
+                        $result = $stmt->get_result();
+
+                        if ($result->num_rows > 0) {
+                            $row = $result->fetch_assoc();
+                            echo htmlspecialchars($row['username']);
+                        } else {
+                            echo "Errore nel recupero del profilo.";
+                        }
+
+                        $stmt->close();
+                        $connection->close();
+                    ?>
+                </p>
+            </div>
+
+            <div class="tab-content">
+                <h3>Cambia la tua password</h3>
+                <form onsubmit="event.preventDefault(); updatePassword();">
+                    <label for="old_password">Vecchia Password:</label>
+                    <input type="password" id="old_password" required>
+
+                    <label for="new_password">Nuova Password:</label>
+                    <input type="password" id="new_password" required>
+
+                    <label for="confirm_new_password">Conferma Nuova Password:</label>
+                    <input type="password" id="confirm_new_password" required>
+
+                    <div id="password-error" style="color: red; display: none; margin-top: 10px;"></div>
+
+                    <button type="submit">Aggiorna Password</button>
                 </form>
-                <div id="delete_container">
-                    <button id="delete-table-btn" onclick="deleteAllTablesMap();">Elimina Tabelle per mappa</button>
-                </div>
-                <!-- Button to delete the table for Application 1 -->
-            </fieldset>
-
-            <!-- Section for Application 2 -->
-            <fieldset>
-                <legend>Votazioni</legend>
-                <form id="upload-form-app2" method="POST" enctype="multipart/form-data">
-                    <label for="json_file">Seleziona il file JSON per visualizzare le votazioni:</label>
-                    <input type="file" id="json_file" name="json_file" accept=".json" required>
-                    <button type="submit">Carica File per votazioni</button>
-                </form>
-                <div id="delete_container">
-                    <button id="delete-table-btn-app2" onclick="deleteAllTablesVotazioni();">Elimina Tabelle votazioni</button>
-                </div>
-            </fieldset>
-        </div>
-
-        <!-- Tab contenuto: Profilo -->
-        <div class="tab-content">
-            <h3>Il tuo profilo</h3>
-            <p>Username: 
-                <?php
-                require "../db_connection.php";
-                session_start(); 
-                if (isset($_SESSION['admin_id'])) {
-                    $adminId = $_SESSION['admin_id'];
-                    $query = "SELECT username FROM admin WHERE id = ?";
-                    $stmt = $connection->prepare($query);
-                    $stmt->bind_param('i', $adminId);
-                    $stmt->execute();
-                    $result = $stmt->get_result();
-
-                    if ($result->num_rows > 0) {
-                        $row = $result->fetch_assoc();
-                        echo htmlspecialchars($row['username']);
-                    } else {
-                        echo "Errore nel recupero del profilo.";
-                    }
-
-                    $stmt->close();
-                    $connection->close();
-                } else {
-                    echo "Non sei loggato.";
-                }
-                ?>
-            </p>
-        </div>
-
-        <!-- Tab contenuto: Modifica password -->
-        <div class="tab-content">
-            <h3>Cambia la tua password</h3>
-            <form onsubmit="event.preventDefault(); updatePassword();">
-                <label for="old_password">Vecchia Password:</label>
-                <input type="password" id="old_password" required>
-
-                <label for="new_password">Nuova Password:</label>
-                <input type="password" id="new_password" required>
-
-                <label for="confirm_new_password">Conferma Nuova Password:</label>
-                <input type="password" id="confirm_new_password" required>
-
-                <div id="password-error" style="color: red; display: none; margin-top: 10px;"></div>
-
-                <button type="submit">Aggiorna Password</button>
-            </form>
-        </div>
-
+            </div>
+        <?php endif; ?>
 
         <!-- Elemento per visualizzare il riepilogo -->
         <div id="summary" style="display: none;">
@@ -120,23 +115,31 @@
         </div>
 
         <!-- Logout -->
-        <p><a href="logout.php">Logout</a></p>
+        <?php if (isset($_SESSION['admin_id'])): ?>
+            <div class="logout_button">
+                <button onclick="window.location.href='logout.php';">Logout</button>
+            </div>
+        <?php else: ?>
+            <div class="dashboard-without-login">
+                <button onclick="window.location.href='logout.php';">Login</button>
+                <button onclick="window.location.href='../';">Visualizza Mappa</button>
+                <button onclick="window.location.href='../votazioni/';">Visualizza Votazioni</button>
+            </div>
+        <?php endif; ?>
     </div>
 
     <!-- Right-side menu for accessing applications -->
-    <div class="sidebar">
-        <button onclick="window.location.href='../';">Visualizza Mappa</button>
-        <button onclick="window.location.href='../votazioni/';">Visualizza Votazioni</button>
-    </div>
+    <?php if (isset($_SESSION['admin_id'])): ?>
+        <div class="sidebar">
+            <button onclick="window.location.href='../';">Visualizza Mappa</button>
+            <button onclick="window.location.href='../votazioni/';">Visualizza Votazioni</button>
+        </div>
+    <?php endif; ?>
 
     <script>
         function closeSummary() {
-            document.getElementById('summary').style.display = 'none'; // Nascondi il riepilogo
+            document.getElementById('summary').style.display = 'none';
         }
-
-
-
-
     </script>
 </body>
 </html>

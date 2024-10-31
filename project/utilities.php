106f8ff91c40cb6c08662476f780149d65304cc9
Andrea Zavatta <zavattaandrea@gmail.com>
2024-10-31 09:59:42 +0100

    remove the dependence of node modules

--- project/utilities.php
+++ project/utilities.php
@@ -2,7 +2,7 @@
 
 function stylesheets(){
     return '
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin=""/>
+    <link rel="stylesheet" href="./js/leaflet.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"/>
     <link rel="stylesheet" href="./css/main.css"/>
     ';
@@ -13,11 +13,11 @@ function scripts(){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <script type="module" src="./js/utilities.js"></script>
-  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+  <script src="./js/leaflet.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   <script src="./js/map.js"></script>
   <script type="module" src="./js/controls.js"></script>
-  <script src="./node_modules/heatmap.js/build/heatmap.js"></script>
+  <script src="./js/heatmap.js"></script>
   <script src="./js/leaflet-heatmap.js"></script>
   <script src="https://unpkg.com/leaflet.markerplayer"></script>
   ';
